bc7e3fca90017fed53ee0c28a9c24e3cc98c113b
Rain <gxl405@case.edu>
2024-04-29 09:14:37 -0400

    '初始化项目'

---
+++ toxPrediction/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ToxpredictionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'toxPrediction'
---
+++ toxinpredWeb/toxpredict.py
@@ -0,0 +1,28 @@
+import argparse
+import warnings
+import pickle
+import os
+import re
+import sys
+import numpy as np
+import pandas as pd
+import joblib
+
+
+# result_filename = id
+# "sequence"
+
+
+def toxPredictionOne(Sequence,result_filename,Threshold,dplay):
+
+    model = 1
+    dplay = 2
+    pass
+
+
+def toxPredictiontwo(Sequence,result_filename,Threshold,dplay):
+    model = 2
+    dplay = 2
+    pass
+
+
